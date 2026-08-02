ccf1bb6bb648c5a4029e2ed4c6a577a14ac12804
coco868 <3206653382@qq.com>
2026-08-02 09:09:43 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'workbench-v7';
+const CACHE = 'workbench-v8';
 const ASSETS = [
   './', './index.html', './style.css', './app.js', './supabase.min.js',
   './manifest.json', './icon-192.png', './icon-512.png', './apple-touch-icon.png'
